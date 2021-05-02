bc94f353829303032b3eac50184529659aceb918
Anuj Singla <asingla@redhat.com>
2021-05-02 12:35:13 +0530

    initial commit .

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { ToggleButton } from "./ToggleButton";
+import { UserForm } from "./UserForm";
+import { Welcome } from "./Welcome";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Welcome name="Anuj" />
+        <Welcome name="Name 1" />
+        <ToggleButton />
+        <UserForm />
       </header>
     </div>
   );
