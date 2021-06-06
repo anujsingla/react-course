b668150d40b0410ea94b500b899926ee87c2c55e
Anuj Singla <asingla@redhat.com>
2021-06-06 12:11:34 +0530

    code changes .

--- src/Components/UseContextReducer.tsx
+++ src/Components/UseContextReducer.tsx
@@ -1,3 +1,19 @@
+// useContext + useReducer
+
+// useContext -> Helps to pass data throughout app without manually passing props down the tree
+// useReducer -> It is similar to useState hook and helps to manage complex state.
+
+// Why do we use useContext and useReducer together?
+
+// Reason: To share states between the components i.e. Global State Management
+
+// In the previous posts, we have seen how to make use of useContext and useReducer hooks.
+
+// So, in this video, we will see how to make use of them together!
+// For small project, we use useContext + useReducer hook
+// For large project (Best practice, not compulsory), we use redux or recoil or other state management library
+
+// UI -> Action -> Dispatch -> state update -> Notify to the UI for state change
 import { map } from "lodash";
 import { useEffect } from "react";
 import { useUserContext } from "../context/UserContextProvider";
